5f1b8aafda9780cdec1f34c96e2ff5652ea71d81
liuhuijun <liuhuijun2017@gmail.com>
2024-03-30 22:54:52 +0800

    feat:constant

---
+++ src/main/java/org/hoey/exception/constant/Constants.java
@@ -0,0 +1,11 @@
+package org.hoey.exception.constant;
+
+public class Constants {
+
+    // 程序流程中，完全不应该触及的分支
+    public static final String UNRECHAED = "Shouldn't be reached";
+
+    // NUT常量用于表示C风格字符串的结尾标志
+    public static final byte NUT = (byte) '\0';
+
+}
--- src/main/java/org/hoey/util/NativeUtil.java
+++ src/main/java/org/hoey/util/NativeUtil.java
@@ -9,14 +9,24 @@ public final class NativeUtil {
 
     public static final MemorySegment NULL_POINTER = MemorySegment.ofAddress(0);
 
-    private static final VarHandle BYTE_HANDLE = MethodHandles.memorySegmentViewVarHandle(ValueLayout.JAVA_BOOLEAN);
+    /**
+     * MethodHandle与VarHandle类均为Java语言自JDK9以后引入的新的反射调用机制，
+     * 其中MethodHandle类可以被理解成函数指针，用于高效的访问函数，
+     * VarHandle类可以被理解成对象指针，用于高效的访问变量。这两个类主要用于替换Java语言中陈旧的反射机制，并且不局限于访问Java中创建的方法与字段，还能够以统一的API实现对于Native方法与堆外内存的访问。
+     */
+    private static final VarHandle BYTE_HANDLE = MethodHandles.memorySegmentViewVarHandle(ValueLayout.JAVA_BYTE);
+
+    private static final VarHandle INT_HANDLER = MethodHandles.memorySegmentViewVarHandle(ValueLayout.JAVA_INT_UNALIGNED);
+
+    private static final VarHandle SHORT_HANDLER = MethodHandles.memorySegmentViewVarHandle(ValueLayout.JAVA_SHORT_UNALIGNED);
+    private static final VarHandle LONG_HANDLER = MethodHandles.memorySegmentViewVarHandle(ValueLayout.JAVA_LONG_UNALIGNED);
 
     public NativeUtil() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
 
     public static boolean isNullPointer(MemorySegment memorySegment) {
-        return memorySegment == null ||memorySegment.address() == 0;
+        return memorySegment == null || memorySegment.address() == 0;
     }
 
     public static byte getByte(MemorySegment memorySegment, long index) {
@@ -26,7 +36,6 @@ public final class NativeUtil {
     public static void setByte(MemorySegment memorySegment, long index, byte value) {
         BYTE_HANDLE.set(memorySegment, index, value);
     }
-
 
 
 }
