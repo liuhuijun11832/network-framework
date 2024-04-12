7958361eb2585b4044d2776c676bf3994c294305
刘会俊 <liuhuijun_2017@163.com>
2024-04-13 00:18:08 +0800

    feat:堆外内存写

--- src/main/java/org/hoey/Main.java
+++ src/main/java/org/hoey/Main.java
@@ -1,6 +1,7 @@
 package org.hoey;
 
 import org.hoey.io.ReadBuffer;
+import org.hoey.io.WriteBuffer;
 import org.hoey.util.NativeUtil;
 import org.slf4j.Logger;
 
@@ -12,7 +13,7 @@ public class Main {
 
     private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try (Arena arena = Arena.ofConfined()) {
             MemorySegment ms = arena.allocate(ValueLayout.JAVA_INT);
             logger.info("MemorySegment address: {}", ms.address());
@@ -26,5 +27,16 @@ public class Main {
             ReadBuffer readBuffer = new ReadBuffer(memorySegment);
             logger.info("Accessing readBuffer : {}", readBuffer.readCStr());
         }
+
+        try (WriteBuffer writeBuffer = WriteBuffer.newDefaultWriteBuffer(Arena.ofConfined(), ValueLayout.JAVA_INT.byteSize())) {
+            writeBuffer.writeInt(1);
+            writeBuffer.writeInt(2);
+            writeBuffer.writeInt(3);
+            MemorySegment segment = writeBuffer.content();
+            logger.info("Accessing index 0: {}", NativeUtil.getInt(segment, 0L));
+            logger.info("Accessing index 4: {}", NativeUtil.getInt(segment, 4L));
+            logger.info("Accessing index 8: {}", NativeUtil.getInt(segment, 8L));
+        }
+
     }
 }
--- src/main/java/org/hoey/io/WriteBuffer.java
+++ src/main/java/org/hoey/io/WriteBuffer.java
@@ -2,8 +2,13 @@ package org.hoey.io;
 
 import org.hoey.exception.ExceptionType;
 import org.hoey.exception.FrameworkException;
+import org.hoey.exception.constant.Constants;
+import org.hoey.util.NativeUtil;
 
+import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.nio.charset.StandardCharsets;
 
 /**
  * 写入缓冲区
@@ -11,7 +16,7 @@ import java.lang.foreign.MemorySegment;
  * @author liuhuijun
  * @since 2024/4/11 9:06
  */
-public class WriteBuffer implements AutoCloseable{
+public final class WriteBuffer implements AutoCloseable {
 
     private static final int DEFAULT_HEAP_BUFFER_SIZE = 32;
     private MemorySegment segment;
@@ -27,13 +32,13 @@ public class WriteBuffer implements AutoCloseable{
     }
 
 
-    public long writeIndex() {
-        return this.writeIndex;
-    }
-
     @Override
     public void close() throws Exception {
+        policy.close(this);
+    }
 
+    public static WriteBuffer newDefaultWriteBuffer(Arena arena, long byteSize) {
+        return new WriteBuffer(arena.allocate(byteSize), new DefaultWriteBufferPolicy(arena));
     }
 
     static final class HeapWriteBufferPolicy implements WriteBufferPolicy {
@@ -42,7 +47,7 @@ public class WriteBuffer implements AutoCloseable{
 
         @Override
         public void resize(WriteBuffer writeBuffer, long nextIndex) {
-            if(nextIndex < 0 || nextIndex > Integer.MAX_VALUE){
+            if (nextIndex < 0 || nextIndex > Integer.MAX_VALUE) {
                 throw new FrameworkException(ExceptionType.NATIVE, "Heap writeBuffer size overflow");
             }
             int newLen = Math.max((int) nextIndex, data.length << 1);
@@ -57,4 +62,194 @@ public class WriteBuffer implements AutoCloseable{
             // heap memory no need close
         }
     }
+
+    record FixedWriBufferPolicy(Arena arena) implements WriteBufferPolicy {
+
+        @Override
+        public void resize(WriteBuffer writeBuffer, long nextIndex) {
+            throw new FrameworkException(ExceptionType.NATIVE, "Current writeBuffer cannot resize");
+        }
+
+        @Override
+        public void close(WriteBuffer writeBuffer) {
+            arena.close();
+        }
+    }
+
+    record DefaultWriteBufferPolicy(Arena arena) implements WriteBufferPolicy {
+
+        @Override
+        public void resize(WriteBuffer writeBuffer, long nextIndex) {
+            // 已有的writeBuffer的大小扩容一倍,和nextIndex比较作为扩容依据
+            long newLen = Math.max(nextIndex, writeBuffer.size << 1);
+            if (newLen < 0) {
+                throw new FrameworkException(ExceptionType.NATIVE, "MemorySize overflow");
+            }
+            // 用新的大小进行堆外分配内存
+            MemorySegment memorySegment = arena.allocateArray(ValueLayout.JAVA_BYTE, newLen);
+            // 内存拷贝操作,类似c语言的memcpy()
+            MemorySegment.copy(writeBuffer.segment, 0, memorySegment, 0, writeBuffer.writeIndex);
+            writeBuffer.segment = memorySegment;
+            writeBuffer.size = newLen;
+        }
+
+        @Override
+        public void close(WriteBuffer writeBuffer) {
+            arena.close();
+        }
+    }
+
+    static final class ReservedWriteBufferPolicy implements WriteBufferPolicy {
+
+        private Arena arena = null;
+
+        @Override
+        public void resize(WriteBuffer writeBuffer, long nextIndex) {
+            long newLen = Math.max(nextIndex, writeBuffer.size << 1);
+            if (newLen < 0) {
+                throw new FrameworkException(ExceptionType.NATIVE, "MemorySize overflow");
+            }
+            if (arena == null) {
+                arena = Arena.ofConfined();
+            }
+            MemorySegment memorySegment = arena.allocateArray(ValueLayout.JAVA_BYTE, newLen);
+            MemorySegment.copy(writeBuffer.segment, 0, memorySegment, 0, writeBuffer.writeIndex);
+            writeBuffer.segment = memorySegment;
+            writeBuffer.size = newLen;
+        }
+
+        @Override
+        public void close(WriteBuffer writeBuffer) {
+            if (arena != null) {
+                arena.close();
+            }
+        }
+    }
+
+    public long size() {
+        return this.size;
+    }
+
+    public long writeIndex() {
+        return this.writeIndex;
+    }
+
+    public void resize(long nextIndex) {
+        if (nextIndex < 0) {
+            throw new FrameworkException(ExceptionType.NATIVE, "Index overflow");
+        } else if (nextIndex > size) {
+            policy.resize(this, nextIndex);
+        }
+    }
+
+    public void writeByte(byte b) {
+        long nextIndex = this.writeIndex + 1;
+        resize(nextIndex);
+        NativeUtil.setByte(segment, writeIndex, b);
+        writeIndex = nextIndex;
+    }
+
+    public void writeBytes(byte[] b, int off, int len) {
+        if (len < 0 || len > b.length - off) {
+            throw new FrameworkException(ExceptionType.NATIVE, "Length overflow");
+        }
+        long nextIndex = this.writeIndex + len;
+        resize(nextIndex);
+        MemorySegment.copy(MemorySegment.ofArray(b), off, segment, writeIndex, len);
+        writeIndex = nextIndex;
+    }
+
+    public void writeBytes(byte... bytes) {
+        long nextIndex = writeIndex + bytes.length;
+        resize(nextIndex);
+        MemorySegment.copy(MemorySegment.ofArray(bytes), 0, segment, writeIndex, bytes.length);
+        writeIndex = nextIndex;
+    }
+
+    public void writeShort(short s) {
+        long nextIndex = writeIndex + ValueLayout.JAVA_SHORT.byteSize();
+        resize(nextIndex);
+        NativeUtil.setShort(segment, writeIndex, s);
+        writeIndex = nextIndex;
+    }
+
+    public void writeInt(int i) {
+        long nextIndex = writeIndex + ValueLayout.JAVA_INT.byteSize();
+        resize(nextIndex);
+        NativeUtil.setInt(segment, writeIndex, i);
+        writeIndex = nextIndex;
+    }
+
+    public void writeLong(long l) {
+        long nextIndex = writeIndex + ValueLayout.JAVA_LONG.byteSize();
+        resize(nextIndex);
+        NativeUtil.setLong(segment, writeIndex, l);
+        writeIndex = nextIndex;
+    }
+
+    public void writeCStr(String str) {
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        MemorySegment memorySegment = MemorySegment.ofArray(bytes);
+        long len = memorySegment.byteSize();
+        long nextIndex = writeIndex + len + 1;
+        resize(nextIndex);
+        MemorySegment.copy(memorySegment, 0, segment, writeIndex, len);
+        NativeUtil.setByte(segment, writeIndex + len, Constants.NUT);
+        writeIndex = nextIndex;
+    }
+
+    public void writeSegment(MemorySegment memorySegment) {
+        long len = memorySegment.byteSize();
+        long nextIndex = writeIndex + len;
+        resize(nextIndex);
+        MemorySegment.copy(memorySegment, 0, segment, writeIndex, len);
+        writeIndex = nextIndex;
+    }
+
+
+    public void setByte(long index, byte value) {
+        if(index + 1 > writeIndex) {
+            throw new RuntimeException("Index out of bound");
+        }
+        NativeUtil.setByte(segment, index, value);
+    }
+
+    public void setShort(long index, short value) {
+        if(index + 2 > writeIndex) {
+            throw new RuntimeException("Index out of bound");
+        }
+        NativeUtil.setShort(segment, index, value);
+    }
+
+    public void setInt(long index, int value) {
+        if(index + 4 > writeIndex) {
+            throw new RuntimeException("Index out of bound");
+        }
+        NativeUtil.setInt(segment, index, value);
+    }
+
+    public void setLong(long index, long value) {
+        if(index + 8 > writeIndex) {
+            throw new RuntimeException("Index out of bound");
+        }
+        NativeUtil.setLong(segment, index, value);
+    }
+
+
+    public MemorySegment content() {
+        return writeIndex == size ? segment : segment.asSlice(0L, writeIndex);
+    }
+
+    public WriteBuffer truncate(long offset) {
+        if(offset > writeIndex) {
+            throw new FrameworkException(ExceptionType.NATIVE, "Truncate index overflow");
+        }
+        WriteBuffer w = new WriteBuffer(segment.asSlice(offset, size - offset), policy);
+        w.writeIndex = writeIndex - offset;
+        return w;
+    }
+
+
+
+
 }
